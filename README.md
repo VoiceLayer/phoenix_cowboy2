674aef2682084c578184f84769dc36fa1f7820dc
Gary Rennie <gazler@gmail.com>
2017-01-20 00:14:02 +0000

    chore(setup): initial commit
    Based on the work by @potatosalad mentioned in this issue:
    
    https://github.com/elixir-lang/plug/issues/258

---
+++ README.md
@@ -0,0 +1,29 @@
+# PhoenixCowboy2
+
+This is a handler for Cowboy 2 based off of the work on @potatosalad's fork
+available at https://github.com/potatosalad/phoenix/tree/cowboy2
+
+## Installation
+
+  1. Add `phoenix_cowboy2` to your list of dependencies in `mix.exs`:
+
+    ```elixir
+    def deps do
+      [{:phoenix_cowboy2, github: "voicelayer/phoenix_cowboy2"}]
+    end
+    ```
+
+  2. Ensure `phoenix_cowboy2` is started before your application:
+
+    ```elixir
+    def application do
+      [applications: [:phoenix_cowboy2]]
+    end
+    ```
+
+## Sample Application
+
+### Phoenix
+
+A sample application with plug is available at
+https://github.com/voicelayer/phoenix_http2_example
---
+++ mix.exs
@@ -0,0 +1,33 @@
+defmodule PhoenixCowboy2.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :phoenix_cowboy2,
+     version: "0.1.0",
+     elixir: "~> 1.3",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    [applications: [:logger, :plug_cowboy2, :phoenix]]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
+  defp deps do
+    [{:plug_cowboy2, github: "voicelayer/plug_cowboy2"},
+     {:phoenix, "~> 1.2"}]
+  end
+end
